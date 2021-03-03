3d9de3e98458165aef22ecd9cc6f02dac409f626
sergiughimpExpleo <78013000+sergiughimpExpleo@users.noreply.github.com>
2021-03-03 16:53:23 +0200

    Customizing Django Admin, Custom User Registration, Login & Logout Users and Check Authentication Status

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
-
-from personal.models import Question
-from django.shortcuts import render
+from account.models import Account
 # Create your views here.
 
 
 def home_screen_view(request):
     context = {}
-    # context['some_string'] = "This is some string that I'm passing to the view"
-    # context['some_number'] = 425332
 
-    # context = {
-    #     'some string': "This is some string that I'm passing to the view",
-    #     'some_number': 425332,
-    # }
-
-    # list_of_values = []
-    # list_of_values.append("First entry")
-    # list_of_values.append("Second entry")
-    # list_of_values.append("Third entry")
-    # list_of_values.append("Fourth entry")
-    # context['list_of_values'] = list_of_values
-
-    questions = Question.objects.all()
-    context['questions'] = questions
+    accounts = Account.objects.all()
+    context['accounts'] = accounts
 
     return render(request, "personal/home.html", context)
